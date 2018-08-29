f6862f29549d370401dd72e07c9f556875ee8896
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-08-29 16:41:00 +0200

    Updates setup.py for namespace packages

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 retval = setup(
     name='quic-tracker-dissector',
     version='0.1',
-    packages=find_packages(),
+    packages=['quic_tracker.dissector'],
+    package_dir={'quic_tracker': 'quic_tracker'},
     url='https://github.com/QUIC-Tracker/dissector',
     license='GNU AGPL v3',
     author='Maxime Piraux',
